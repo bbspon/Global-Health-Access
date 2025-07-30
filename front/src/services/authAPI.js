b6f4f3ff37f23dee5461c39b62f372ed3b7c5d25
bbspon <bbspon@gmail.com>
2025-07-30 11:13:32 +0530

    Update auth url

--- front/src/services/authAPI.js
+++ front/src/services/authAPI.js
@@ -2,8 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/auth", // Adjust to your server address
+  baseURL: `${process.env.REACT_APP_API_URI}/auth`,
 });
-
 export const loginUser = (data) => API.post("/login", data);
 export const signupUser = (data) => API.post("/signup", data);
